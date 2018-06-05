cfdef08b9e9996759c20a7fad437fbbfee9cabae
Marcel Martin <marcel.martin@scilifelab.se>
2018-06-05 22:56:13 +0200

    Cythonize during build and sdist

--- setup.py
+++ setup.py
@@ -1,4 +1,54 @@
+import sys
+import os.path
 from setuptools import setup, Extension
+from distutils.command.sdist import sdist as _sdist
+from distutils.command.build_ext import build_ext as _build_ext
+
+if sys.version_info[:2] < (3, 6):
+    sys.stdout.write('Python 3.6 or later is required\n')
+    sys.exit(1)
+
+
+def no_cythonize(extensions, **_ignore):
+    """Change .pyx to .c or .cpp (copied from Cython documentation)"""
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in ('.pyx', '.py'):
+                if extension.language == 'c++':
+                    ext = '.cpp'
+                else:
+                    ext = '.c'
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+
+
+class build_ext(_build_ext):
+    def run(self):
+        # If we encounter a PKG-INFO file, then this is likely a .tar.gz/.zip
+        # file retrieved from PyPI that already includes the pre-cythonized
+        # extension modules, and then we do not need to run cythonize().
+        if os.path.exists('PKG-INFO'):
+            no_cythonize(extensions)
+        else:
+            # Otherwise, this is a 'developer copy' of the code, and then the
+            # only sensible thing is to require Cython to be installed.
+            from Cython.Build import cythonize
+            self.extensions = cythonize(self.extensions)
+        _build_ext.run(self)
+
+
+class sdist(_sdist):
+    def run(self):
+        # Make sure the compiled Cython files in the distribution are up-to-date
+        from Cython.Build import cythonize
+        cythonize(extensions)
+        _sdist.run(self)
+
+
+extensions = [Extension('dnaio._core', sources=['src/dnaio/_core.pyx'])]
 
 setup(
     name='dnaio',
@@ -11,10 +61,11 @@ setup(
     license='MIT',
     packages=['dnaio'],
     package_dir={'': 'src'},
-    ext_modules=[Extension('dnaio._core', sources=['src/dnaio/_core.pyx'])],
     extras_require={
         'dev': ['Cython', 'pytest'],
     },
+    ext_modules=extensions,
+    cmdclass={'sdist': sdist, 'build_ext': build_ext},
     install_requires=['xopen'],
     classifiers=[
             "Development Status :: 1 - Planning",
