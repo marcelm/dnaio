aa2fdcbb9e7beb089fbd0d42bdeabb59207622ab
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2021-09-14 09:32:00 +0200

    Replace fastq bytes method, add sequence_bytes and qualities_bytes methods

--- dnaio_read.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import dnaio
-import sys
-
-with dnaio.open(sys.argv[1]) as records:
-    for record in records:
-        pass
--- src/dnaio/_core.pyx
+++ src/dnaio/_core.pyx
@@ -1,7 +1,6 @@
 # cython: language_level=3, emit_code_comments=False
 
-from cpython.bytes cimport PyBytes_FromStringAndSize, PyBytes_AS_STRING, PyBytes_GET_SIZE
-from cpython.unicode cimport PyUnicode_AsASCIIString, PyUnicode_Check
+from cpython.bytes cimport PyBytes_FromStringAndSize, PyBytes_AS_STRING
 from libc.string cimport strncmp, memcmp, memcpy
 cimport cython
 
@@ -64,70 +63,23 @@ cdef class Sequence:
     def __reduce__(self):
         return (Sequence, (self.name, self.sequence, self.qualities))
 
+    def sequence_bytes(self):
+        """Returns the sequence as a bytes object."""
+        return self.sequence.encode('ascii')
+
     def qualities_bytes(self):
+        """Returns the qualities as a bytes object"""
         return self.qualities.encode('ascii')
 
     def fastq_bytes(self):
-        s = ('@' + self.name + '\n' + self.sequence + '\n+\n'
-             + self.qualities + '\n')
-        return s.encode('ascii')
-
-    def fastq_bytes2(self):
-        cdef bytes name = self.name.encode('ascii')
-        cdef bytes sequence = self.sequence.encode('ascii')
-        cdef bytes qualities = self.qualities.encode('ascii')
-        cdef bytes RetVal = b"@" + name + b"\n" + sequence + b"\n+\n" + qualities + b"\n"
-        return RetVal
-
-    def fastq_bytes3(self):
-        cdef bytes name = self.name.encode('ascii')
-        cdef bytes sequence = self.sequence.encode('ascii')
-        cdef bytes qualities = self.qualities.encode('ascii')
-        cdef bytes RetVal = b"".join([b"@", name, b"\n", sequence,
-        b"\n+\n", qualities, b"\n"])
-        return RetVal
-
-    def fastq_bytes4(self):
+        """Returns the entire fastq record as bytes which can be written
+        into a file."""
         cdef bytes name = self.name.encode('ascii')
         cdef bytes sequence = self.sequence.encode('ascii')
         cdef bytes qualities = self.qualities.encode('ascii')
         cdef Py_ssize_t name_length = len(name)
         cdef Py_ssize_t sequence_length = len(sequence)
         cdef Py_ssize_t qualities_length = len(qualities)
-        cdef char * name_ptr = PyBytes_AS_STRING(name)
-        cdef char * sequence_ptr = PyBytes_AS_STRING(sequence)
-        cdef char * qualities_ptr = PyBytes_AS_STRING(qualities)
-        cdef Py_ssize_t total_size = name_length + sequence_length + qualities_length + 6
-        cdef Py_ssize_t cursor
-        cdef bytes RetVal = PyBytes_FromStringAndSize(NULL, total_size)
-        cdef char * retval_ptr = PyBytes_AS_STRING(RetVal)
-        retval_ptr[0] = b"@"
-        memcpy(retval_ptr + 1, name_ptr, name_length)
-        cursor = name_length + 1
-        retval_ptr[cursor] = b"\n"; cursor += 1
-        memcpy(retval_ptr + cursor, sequence_ptr, sequence_length)
-        cursor += sequence_length
-        retval_ptr[cursor] = b"\n"; cursor += 1
-        retval_ptr[cursor] = b"+"; cursor += 1
-        retval_ptr[cursor] = b"\n"; cursor += 1
-        memcpy(retval_ptr + cursor, qualities_ptr, qualities_length)
-        cursor += qualities_length
-        retval_ptr[cursor] = b"\n"
-        return RetVal
-
-    def fastq_bytes5(self):
-        if not PyUnicode_Check(self.name):
-            raise TypeError("Name should be of type: str")
-        if not PyUnicode_Check(self.sequence):
-            raise TypeError("Sequence should be of type: str")
-        if not PyUnicode_Check(self.qualities):
-            raise TypeError("Qualities should be of type: str")
-        cdef bytes name = PyUnicode_AsASCIIString(self.name)
-        cdef bytes sequence = PyUnicode_AsASCIIString(self.sequence)
-        cdef bytes qualities = PyUnicode_AsASCIIString(self.qualities)
-        cdef Py_ssize_t name_length = PyBytes_GET_SIZE(name)
-        cdef Py_ssize_t sequence_length = PyBytes_GET_SIZE(sequence)
-        cdef Py_ssize_t qualities_length = PyBytes_GET_SIZE(qualities)
         cdef char * name_ptr = PyBytes_AS_STRING(name)
         cdef char * sequence_ptr = PyBytes_AS_STRING(sequence)
         cdef char * qualities_ptr = PyBytes_AS_STRING(qualities)
