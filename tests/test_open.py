74c58fd7337d8325063a804272a30c6dd4383979
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-01-18 10:35:13 +0100

    Add tests for writing sequence bytes

--- tests/test_open.py
+++ tests/test_open.py
@@ -31,12 +31,17 @@ SIMPLE_RECORDS = {
 def formatted_sequence(record, fileformat):
     if fileformat == "fastq":
         return "@{}\n{}\n+\n{}\n".format(record.name, record.sequence, record.qualities)
+    elif fileformat == "fastq_bytes":
+        return b"@%b\n%b\n+\n%b\n" % (record.name, record.sequence, record.qualities)
     else:
         return ">{}\n{}\n".format(record.name, record.sequence)
 
 
 def formatted_sequences(records, fileformat):
-    return "".join(formatted_sequence(record, fileformat) for record in records)
+    record_iter = (formatted_sequence(record, fileformat) for record in records)
+    if fileformat == "fastq_bytes":
+        return b"".join(record_iter)
+    return "".join(record_iter)
 
 
 def test_formatted_sequence():
@@ -170,8 +175,9 @@ def test_detect_compressed_fastq_from_content():
     assert record.name == 'prefix:1_13_573/1'
 
 
-def test_write(tmpdir, extension):
-    s = dnaio.Sequence('name', 'ACGT', 'HHHH')
+@pytest.mark.parametrize("s", [dnaio.Sequence('name', 'ACGT', 'HHHH'),
+                               dnaio.SequenceBytes(b'name', b'ACGT', b'HHHH')])
+def test_write(s, tmpdir, extension):
     out_fastq = tmpdir.join("out.fastq" + extension)
     with dnaio.open(str(out_fastq), mode='w') as f:
         f.write(s)
@@ -233,6 +239,27 @@ def test_write_paired(tmpdir, fileformat, extension):
         assert formatted_sequences(r1, fileformat) == f.read()
     with xopen(path2) as f:
         assert formatted_sequences(r2, fileformat) == f.read()
+
+
+def test_write_paired_binary(tmpdir, extension):
+    r1 = [
+        dnaio.SequenceBytes(b"s1", b"ACGT", b"HHHH"),
+        dnaio.SequenceBytes(b"s2", b"CGCA", b"8383"),
+    ]
+    r2 = [
+        dnaio.SequenceBytes(b"t1", b"TCGT", b"5HHH"),
+        dnaio.SequenceBytes(b"t2", b"TGCA", b"5383"),
+    ]
+    path1 = str(tmpdir / ("out.1.fastq" + extension))
+    path2 = str(tmpdir / ("out.2.fastq" + extension))
+
+    with dnaio.open(path1, file2=path2, fileformat="fastq", mode="w") as f:
+        f.write(r1[0], r2[0])
+        f.write(r1[1], r2[1])
+    with xopen(path1, "rb") as f:
+        assert formatted_sequences(r1, "fastq_bytes") == f.read()
+    with xopen(path2, "rb") as f:
+        assert formatted_sequences(r2, "fastq_bytes") == f.read()
 
 
 def test_write_interleaved(tmpdir, fileformat, extension):
