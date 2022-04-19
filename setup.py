a6ff28e5134c92d19e6c54f4e2b868bb726acf13
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-04-19 15:04:23 +0200

    Simplify DEFINE_MACROS condition

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ from setuptools import setup, Extension
 import setuptools_scm  # noqa  Ensure it’s installed
 
 # Check for 64-bit
-if sys.maxsize > 2**32 and (
-    platform.machine() == "x86_64" or platform.machine() == "AMD64"
-):
+if platform.machine() == "x86_64" or platform.machine() == "AMD64":
     DEFINE_MACROS = [("USE_SSE2", None)]
 else:
     DEFINE_MACROS = []
