bb852442fb53d9735e762a66181915ba534a3d97
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-07-19 11:20:22 +0200

    Start on multiple writer

--- src/dnaio/multipleend.py
+++ src/dnaio/multipleend.py
@@ -7,22 +7,23 @@ from ._core import SequenceRecord, records_are_mates
 from .exceptions import FileFormatError
 from .readers import FastaReader, FastqReader
 from .singleend import _open_single
+from .writers import FastaWriter, FastqWriter
 
 
 class MultipleFileReader:
     def __init__(
         self,
-        files: Iterable[Union[str, PathLike, BinaryIO]],
+        *files: Union[str, PathLike, BinaryIO],
         fileformat: Optional[str] = None,
         opener=xopen,
     ):
-        self.files = tuple(files)
-        self.number_of_files = len(self.files)
-        if self.number_of_files < 1:
+        if len(files) < 1:
             raise ValueError("At least one file is required")
+        self.files = files
         self.readers: List[Union[FastaReader, FastqReader]] = [
-            _open_single(file, opener=opener, fileformat=fileformat, mode="r"
-                         )  # type: ignore
+            _open_single(
+                file, opener=opener, fileformat=fileformat, mode="r"
+            )  # type: ignore
             for file in self.files
         ]
         self.delivers_qualities: bool = self.readers[0].delivers_qualities
@@ -34,9 +35,9 @@ class MultipleFileReader:
         )
 
     def __iter__(self) -> Iterator[Tuple[SequenceRecord, ...]]:
-        if self.number_of_files == 1:
+        if len(self.files) == 1:
             return zip(self.readers[0])
-        elif self.number_of_files == 2:
+        elif len(self.files) == 2:
             for record1, record2 in zip(*self.readers):
                 if not record1.is_mate(record2):
                     raise FileFormatError(
@@ -72,3 +73,38 @@ class MultipleFileReader:
     def close(self):
         for reader in self.readers:
             reader.close()
+
+
+class MultipleFileWriter:
+    def __init__(
+        self,
+        *files: Union[str, PathLike, BinaryIO],
+        fileformat: Optional[str] = "fastq",
+        qualities: Optional[bool] = None,
+        opener=xopen,
+        append: bool = False,
+    ):
+        if len(files) < 1:
+            raise ValueError("At least one file is required")
+        mode = "a" if append else "w"
+        self.files = files
+        self.writers: List[Union[FastaWriter, FastqWriter]] = [
+            _open_single(
+                file,
+                opener=opener,
+                fileformat=fileformat,
+                mode=mode,
+                qualities=qualities,
+            )  # type: ignore
+            for file in self.files
+        ]
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}"
+            f"({', '.join(repr(writer) for writer in self.writers)})"
+        )
+
+    def close(self):
+        for writer in self.writers:
+            writer.close()
