7b563384fc0afc02416ce433ac11ba047d135bb2
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-07-19 09:16:02 +0200

    Add interfaces

--- src/dnaio/interfaces.py
+++ src/dnaio/interfaces.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Iterator, Tuple
+from typing import Iterable, Iterator, Tuple
 
 from dnaio import SequenceRecord
 
@@ -22,6 +22,19 @@ class PairedEndReader(ABC):
         """
 
 
+class MultipleFileReader(ABC):
+    @abstractmethod
+    def __iter__(self) -> Iterator[Tuple[SequenceRecord, ...]]:
+        """
+        Yields the records as N-tuples depending on the number of input files.
+
+        If there are two or more files, it will raise a `FileFormatError`
+        if reads are improperly paired. That is when there are more reads in
+        one file than the others or if the record IDs do not match (according
+        to `SequenceRecord.is_mate` or `records_are_mates`).
+        """
+
+
 class SingleEndWriter(ABC):
     @abstractmethod
     def write(self, record: SequenceRecord) -> None:
@@ -39,4 +52,26 @@ class PairedEndWriter(ABC):
         that the record IDs no longer match, check that
         ``record1.is_mate(record2)`` returns True before calling
         this function.
+        """
+
+
+class MultipleFileWriter(ABC):
+    number_of_files: int
+
+    @abstractmethod
+    def write(self, records: Tuple[SequenceRecord, ...]) -> None:
+        """
+        Write a N-tuple of SequenceRecords to the output. N should be equal
+        to the number of files the MultipleFileWriter was initiated with.
+
+        This method does not check wheter the records are properly paired.
+        """
+
+    def write_list(self, list_of_records: Iterable[Tuple[SequenceRecord, ...]]):
+        """
+        Iterates over the list (or other iterable container) and writes all
+        N-tuples of SequenceRecord to disk. N should be equal
+        to the number of files the MultipleFileWriter was initiated with.
+
+        This method does not check wheter the records are properly paired.
         """
