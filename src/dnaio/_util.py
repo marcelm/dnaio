02d7d89988f96949b42d2cc2f6c1f29573507798
Marcel Martin <marcel.martin@scilifelab.se>
2022-10-10 18:11:21 +0200

    Get rid of pathlib2 reference

--- src/dnaio/_util.py
+++ src/dnaio/_util.py
@@ -3,20 +3,9 @@ import pathlib
 
 def _is_path(obj: object) -> bool:
     """
-    Return whether the given object looks like a path (str, pathlib.Path or pathlib2.Path)
+    Return whether the given object looks like a path (str or pathlib.Path)
     """
-    # TODO
-    # pytest uses pathlib2.Path objects on Python 3.5 for its tmp_path fixture.
-    # On Python 3.6+, this function can be replaced with isinstance(obj, os.PathLike)
-    import sys
-
-    if "pathlib2" in sys.modules:
-        import pathlib2  # type: ignore
-
-        path_classes = [str, pathlib.Path, pathlib2.Path]
-    else:
-        path_classes = [str, pathlib.Path]
-    return isinstance(obj, tuple(path_classes))
+    return isinstance(obj, (str, pathlib.Path))
 
 
 def shorten(s: str, n: int = 100) -> str:
