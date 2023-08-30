03af5ea5a60098985706ad5a854db2c6a680b50d
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-08-30 15:16:29 +0200

    Make sure comment and id are properly updated when name is updated

--- tests/test_records.py
+++ tests/test_records.py
@@ -141,6 +141,14 @@ class TestSequenceRecord:
     def test_get_id(self, record, expected):
         assert record.id == expected
 
+    def test_reset_id_and_comment_on_name_update(self):
+        record = SequenceRecord("Obi-Wan: don't try it!", "", "")
+        assert record.id == "Obi-Wan:"
+        assert record.comment == "don't try it!"
+        record.name = "Anakin: you underestimate my power!"
+        assert record.id == "Anakin:"
+        assert record.comment == "you underestimate my power!"
+
 
 def test_legacy_sequence():
     from dnaio import Sequence
