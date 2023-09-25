7e8be763798e02ddeb18407858cd006bdd37a1c2
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-09-25 15:33:27 +0200

    Use SSSE3 on linux x86_64

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
+import os
 import platform
+import sys
 
 from setuptools import setup, Extension
 import setuptools_scm  # noqa  Ensure it’s installed
@@ -10,10 +12,23 @@ if platform.machine() == "AMD64":
 else:
     DEFINE_MACROS = []
 
+
+def extra_compile_args():
+    if sys.platform.startswith("linux") and platform.machine() == "x86_64":
+        # DNAIO_CPU_BASIC=1 pip install --no-binary dnaio dnaio
+        # Will work for linux users that do not have SSSE3 support.
+        if not os.getenv("DNAIO_CPU_BASIC"):
+            return ["-mssse3"]
+    # Do not bother with Windows and MacOS as it is not given that they have
+    # a compiler installed. Simply use compatible wheels instead so no users
+    # run into trouble.
+    return None
+
 setup(
     ext_modules=[
         Extension(
-            "dnaio._core", sources=["src/dnaio/_core.pyx"], define_macros=DEFINE_MACROS
+            "dnaio._core", sources=["src/dnaio/_core.pyx"],
+            define_macros=DEFINE_MACROS, extra_compile_args=extra_compile_args()
         ),
     ],
 )
