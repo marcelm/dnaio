055142729c0a820dcd9f8f5408fc697e808a86cb
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-10-17 13:23:47 +0200

    Fix typo in test

--- tests/test_internal.py
+++ tests/test_internal.py
@@ -711,7 +711,6 @@ class TestRecordsAreMates:
 class TestBamReader:
     bam_file = (
         TEST_DATA
-        / "data"
         / "project.NIST_NIST7035_H7AP8ADXX_TAAGGCGA_1_NA12878"
         ".bwa.markDuplicates.bam"
     )
